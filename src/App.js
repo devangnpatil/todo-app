0af0696dbd6914d8bcd50a90672f44a6dbf4c003
devang <devang.patil@upgrad.com>
2019-02-20 16:24:43 +0530

    add todo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import Todos from './Todos'
 class App extends Component {
   state = {
-    todo:[
+    todos:[
       {id:1, content:'Buy some milk'},
       {id:2, content:'Play Cricket'}
     ]
   }
+
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+  }
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todos's</h1>
-        <Todos todos={this.state.todo} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
